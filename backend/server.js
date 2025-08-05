652633676d3aa0d39352c51a5d429f88c8532134
Gurmesa <gurmesakedir96@gmail.com>
2025-08-05 23:18:30 +0300

    this product mongoose db code for product and create api by express js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/connectDB.js';
+import product from './models/product.model.js'; 
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
-app.post('/products', async (req, res) => {
-  res.send("server is ready12");
+app.post('/api/products', async (req, res) => {
+  const product = req.body;
+
+  if(!product.name || !product.price || !product.image){
+    return res.status(400).json({success : false, message: "please provide all fields"})
+  }
+
+  const newproduct = new product(product);
+
+  try{
+    await newproduct.save();
+    res.status(201).json({success: true, data: newproduct});
+  }catch(error){
+    console.log("Error in create product :", error.message);
+    res.status(500).json({success: false, message: "server Error"})
+  }
+
 });
 
 app.listen(5000, () => {
